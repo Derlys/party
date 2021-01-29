c0de00494b0d0dde263cde6a5dc317c5b33c4d78
Derlys <derlys@192.168.1.6>
2021-01-28 22:20:26 -0500

    add music plugin

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from "@angular/core";
-
-import { Item } from "./item";
-import { ItemService } from "./item.service";
-
+import { TNSPlayer } from 'nativescript-audio';
 @Component({
     selector: "ns-items",
-    templateUrl: "./items.component.html"
+    moduleId: module.id,
+    templateUrl: "./items.component.html",
 })
 export class ItemsComponent implements OnInit {
-    items: Array<Item>;
+    private _player: TNSPlayer;
+    constructor() {
+        this._player = new TNSPlayer();
+        this._player.initFromFile({
+            audioFile: '~/app/audio.mp3', // ~ = root directory
+            loop: false,
+            completeCallback: this._trackComplete.bind(this),
+            errorCallback: this._trackError.bind(this)
+        }).then(() => {
+            this._player.getAudioTrackDuration().then(duration => {
+                // iOS: duration is in seconds
+                // Android: duration is in milliseconds
+                console.log(`song duration:`, duration);
+            });
+        });
+    }
 
-    constructor(private itemService: ItemService) { }
+    ngOnInit(): void {}
 
-    ngOnInit(): void {
-        this.items = this.itemService.getItems();
+    public togglePlay() {
+        if (this._player.isAudioPlaying()) {
+            this._player.pause();
+        } else {
+            this._player.play();
+        }
+    }
+
+    private _trackComplete(args: any) {
+        console.log('reference back to player:', args.player);
+        console.log('whether song play completed successfully:', args.flag);
+    }
+
+    private _trackError(args: any) {
+        console.log('reference back to player:', args.player);
+        console.log('the error:', args.error);
+        console.log('extra info on the error:', args.extra);
     }
 }
