f98dd8a5cf326e320ddcf00bcf86354dc10d6316
Derlys <derlys@192.168.1.6>
2021-01-29 22:50:55 -0500

    add animation

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import { TNSPlayer } from 'nativescript-audio';
+
 @Component({
     selector: "ns-items",
     moduleId: module.id,
     templateUrl: "./items.component.html",
 })
+
 export class ItemsComponent implements OnInit {
+    @ViewChild('animateImg') animageImgEl: ElementRef;
+    public img;
     private _player: TNSPlayer;
+    public partying: boolean;
     constructor() {
         this._player = new TNSPlayer();
         this._player.initFromFile({
@@ -23,14 +28,9 @@ export class ItemsComponent implements OnInit {
         });
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.img = this.animageImgEl.nativeElement;
 
-    public togglePlay() {
-        if (this._player.isAudioPlaying()) {
-            this._player.pause();
-        } else {
-            this._player.play();
-        }
     }
 
     private _trackComplete(args: any) {
@@ -42,5 +42,37 @@ export class ItemsComponent implements OnInit {
         console.log('reference back to player:', args.player);
         console.log('the error:', args.error);
         console.log('extra info on the error:', args.extra);
+    }
+    public togglePlay() {
+        if (this._player.isAudioPlaying()) {
+            this._player.pause();
+            this.partying = false;
+            this.stopAnimation();
+        } else {
+            this._player.play();
+            this.partying = true;
+            this.startAnimation();
+        }
+    }
+
+    private animateInterval;
+    startAnimation() {
+        this.animateInterval = setInterval(() => {
+            this.img.animate({
+                scale: {x: 2, y: 2},
+                rotate: 45,
+                duration: 270
+            }).then(() => {
+                this.img.animate({
+                    scale: {x: 1, y: 1},
+                    rotate: 0,
+                    duration: 270
+                }).then(() => {}, err => {})
+            }, err => {})
+        }, 540)
+    }
+
+    stopAnimation() {
+        clearInterval(this.animateInterval);
     }
 }
